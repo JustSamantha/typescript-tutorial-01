2ce6e13ec3043ffbd5b811d856ec4752893b084a
Samantha Castillo <samanthacas@pm.me>
2025-01-19 15:33:03 -0800

    - Adds build folder to the gitignore - Adds a package.jsom - Adds a typescript config file - Adds demo code

---
+++ src/demo.ts
@@ -0,0 +1,23 @@
+interface Contact extends Address {
+  id:number;
+  name:string;
+  birthdate?:Date;
+}
+
+interface Address {
+  line1:string;
+  line2?:string;
+  city:string;
+  state:string;
+  postalCode:string;
+}
+
+let primaryContact:Contact = {
+  id: 123456,
+  name: "Samantha Castillo",
+  line1: "address line 1",
+  line2: "address line 2",
+  city: "my city",
+  state: "CA",
+  postalCode: "98763-89",
+}
