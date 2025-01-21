1a17cc45e7471720816016d86e28bd4e9cd909d3
Samantha Castillo <samanthacas@pm.me>
2025-01-21 11:08:01 -0800

    - Adds build forlder to the gitignore - Adds a node.js confoguration file - Adds a typescript configuration file - Adds demo code - Fixes the clone function to do a deep clone

--- src/demo.ts
+++ src/demo.ts
@@ -3,6 +3,19 @@ interface Contact {
     name: string;
 }
 
-function clone(source) {
-    return Object.apply({}, source);
+function clone(source:Contact):Contact {
+    return JSON.parse(JSON.stringify(source));
 }
+
+let myContact:Contact = {
+  id: 1,
+  name: "Samantha Castillo",
+}
+
+let myOtherContact:Contact = clone(myContact);
+
+myContact.id = 2;
+myOtherContact.id = 3;
+
+console.log(myContact);
+console.log(myOtherContact);
