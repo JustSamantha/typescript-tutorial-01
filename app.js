4bf116a675f2104fb8ea86e9c2216da3af0c3b74
Samantha Castillo <samanthacas@pm.me>
2025-01-27 09:47:58 -0800

    - Adds demo code

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-function query<T>(
-    items: T[],
-    query: {
-        [TProp in keyof T]?: (val: T[TProp]) => boolean
-    }
-) {
+function query(items, query) {
     return items.filter(item => {
         // iterate through each of the item's properties
         for (const property of Object.keys(item)) {
-
             // get the query for this property name
-            const propertyQuery = query[property]
-
+            const propertyQuery = query[property];
             // see if this property value matches the query
             if (propertyQuery && propertyQuery(item[property])) {
-                return true
+                return true;
             }
         }
-
         // nothing matched so return false
-        return false
-    })
+        return false;
+    });
 }
-
-const matches = query(
-    [
-        { name: "Ted", age: 32 },
-        { name: "Angie", age: 31 }
-    ],
-    {
-        name: name => name === "Angie",
-        age: age => age > 30
-    })
-
+const matches = query([
+    { name: "Ted", age: 32 },
+    { name: "Angie", age: 31 }
+], {
+    name: name => name === "Angie",
+    age: age => age > 30
+});
 console.log(matches);
