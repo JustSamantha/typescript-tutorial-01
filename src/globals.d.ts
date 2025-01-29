1fb5ac32d3acaa9af4d2c32695fc1ac74d53539c
Your Name <you@example.com>
2025-01-29 15:52:39 -0800

    - Adds dist folder to the gitignore file - Changed app.js to typescript - Declared global function

---
+++ src/globals.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  /**
+   * This formats a date valie to a human-readable format
+   * @param date The date to format
+   * @returns A string with the date formatted
+   */
+  function formatDate(date: Date): string
+}
+
+export {}
